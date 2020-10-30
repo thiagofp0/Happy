735a25ec1e829ee56673bed9d74443bda76659c5
Thiago <thiagofp707@gmail.com>
2020-10-30 01:41:59 -0300

    Landing page pronta

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
 import './styles/global.css';
 import './styles/pages/landing.css';
+
+import logoImg from "./assets/img/logo.svg";
 
 function App() {
   return (
     <div id="page-landing">
-      <h1>Hello TH</h1>
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo.</h1>
+          <p>Visite orfanatos e mude o dia de mauitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Ipatinga</strong>
+          <span>Minas Gerais</span>
+        </div>
+
+        <a href=""className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
+        </a>
+      </div>      
     </div>
   );
 }
